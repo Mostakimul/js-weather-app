f90efe8b2e510927e9fe2e129d790da45d5035e9
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-07-26 01:32:47 +0200

    updated card details and hide show

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 // getting reference
 const cityForm = document.querySelector('.change-location');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+// Update UI function
+const updateUI = (data) => {
+  // console.log(data);
+  const locationDetails = data.locationDetails;
+  const weather = data.weather;
+
+  // details
+  details.innerHTML = `
+  <h5 class="my-3">${locationDetails.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  </div>
+  `;
+
+  // removing d-none clas
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
 
 // API calling here
 const updateCity = async (location) => {
@@ -25,7 +49,8 @@ cityForm.addEventListener('submit', (e) => {
   // updating UI
   updateCity(location)
     .then((data) => {
-      console.log(data);
+      // console.log(data);
+      updateUI(data);
     })
     .catch((err) => {
       console.log(err);
